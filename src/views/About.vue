70d50f3eb6a3252af3765dd7e61ca8e01809e9fa
zhangqiongyu <zhangqythu@gmail.com>
2017-04-13 02:37:53 +0800

    update about page

--- src/views/About.vue
+++ src/views/About.vue
@@ -2,8 +2,12 @@
   <div class="about">
     <navbar></navbar>
     <div class="content">
-    <h4>本网站仅用于个人编写博客、展示简历、记录日常学习工作。网站已做备案，如有问题，请联系本人。</h4>
-    <h4>网站使用vuejs搭建，博客使用django搭建，利用nginx部署到主机。博客使用markdown书写，主要分为前端和后端。</h4>
+      <h3>本网站仅用于个人编写博客、展示简历、记录日常学习工作，网站已做备案。</h3>
+      <p>网站使用vuejs编写，博客使用django搭建，利用uWSGI+nginx部署。博客使用markdown渲染，
+        内容主要包括本人在学习前端、后端过程中的一些学习笔记，或者一些随笔日记。本网站目的在于将
+        平时所学不定期更新上来，目前暂不打算开通评论、注册功能，因此，您所见，即所有。</p>
+      <p>欲与本站站主取得联系，交流学习经验，请上本人github账号下留言，其余联系方式，出于隐私考
+        虑，暂放个占位符，来来往往皆为客，在此感谢您的光临。</p>
     </div>
     <page-footer></page-footer>
   </div>
@@ -27,6 +31,10 @@ export default {
 </script>
 
 <style scoped>
+@font-face {
+  font-family: "BitterRegular";
+  src: url(../assets/fonts/Bitter-Regular-webfont.woff);
+}
 .about {
   background-image: url(../assets/image/aboutBg.jpg);
   background-size: 100% 100%;
@@ -36,8 +44,18 @@ export default {
   height: 450px;
 }
 
-h4 {
-  text-align: center;
+h3 {
+  font-size: 1.45em;
+  font-family: "BitterRegular", Georgia, serif;
   color: white;
+  text-align: center;
+}
+
+p {
+  color: white;
+  font-family: "BitterRegular", Georgia, serif;
+  font-size: 1.1em;
+  padding: 0 10%;
+  text-indent: 2.2em;
 }
 </style>
